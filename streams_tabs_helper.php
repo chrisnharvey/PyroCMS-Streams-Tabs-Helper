e657be0e9c47969239fac0bebda7803331599c98
Chris Harvey <chris@chrisnharvey.com>
2013-07-17 13:36:14 +0100

    Adding doc blocks

--- streams_tabs_helper.php
+++ streams_tabs_helper.php
@@ -1,5 +1,20 @@
 <?php defined('BASEPATH') or exit('No direct script access allowed');
+/**
+ * @package  PyroCMS
+ * @subpackage Streams Tabs Helper
+ * @author Chris Harvey <chris@chrisnharvey.com>
+ * @license MIT
+ */
 
+/**
+ * Build a tabs array for streams
+ * 
+ * @param  array  $tabs       Your associative tab array
+ * @param  string $stream     Stream slug
+ * @param  string $namespace  Stream namesapce
+ * @param  string $default    The default tab where other fields will go if they have not been assigned to a tab
+ * @return array              The tabs array ready to be passed into $this->streams->cp->entry_form()
+ */
 function build_tabs($tabs, $stream, $namespace = null, $default = 'general')
 {
     $fields = ci()->streams->streams->get_assignments($stream, $namespace);
